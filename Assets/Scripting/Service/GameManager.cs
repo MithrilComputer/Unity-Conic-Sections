dc0b9a41432de73b68049379904b395659c02930
MithrilComputer <mithrilcomputers@gmail.com>
2025-07-06 16:31:49 -0400

    General Update
    - Added better documentation.
    - Other minor stuff

--- Assets/Scripting/Service/GameManager.cs
+++ Assets/Scripting/Service/GameManager.cs
@@ -1,16 +1,44 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TMP_Dropdown conicSelection;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private GameObject circleOptions;
+    [SerializeField] private Slider circleHSlider;
+    [SerializeField] private Slider circleKSlider;
+    [SerializeField] private Slider circleRSlider;
+
+    [SerializeField] private GameObject parabolaOptions;
+    [SerializeField] private Slider parabolaHSlider;
+    [SerializeField] private Slider parabolaKSlider;
+    [SerializeField] private Slider parabolaPSlider;
+    [SerializeField] private Button parabolaVerticalButton;
+    [SerializeField] private Button parabolaHorizontalButton;
+
+    [SerializeField] private GameObject ellipseOptions;
+    [SerializeField] private Slider ellipseHSlider;
+    [SerializeField] private Slider ellipseKSlider;
+    [SerializeField] private Slider ellipseASlider;
+    [SerializeField] private Slider ellipseBSlider;
+
+    [SerializeField] private GameObject hyperbolaOptions;
+    [SerializeField] private Slider hyperbolaHSlider;
+    [SerializeField] private Slider hyperbolaKSlider;
+    [SerializeField] private Slider hyperbolaASlider;
+    [SerializeField] private Slider hyperbolaBSlider;
+    [SerializeField] private Slider hyperbolaCSlider;
+    [SerializeField] private Button hyperbolaHorizontalButton;
+    [SerializeField] private Button hyperbolaVerticalButton;
+
+    private void Start()
     {
-        
+        // Initialize UI elements or set up event listeners here if needed
+        circleOptions.SetActive(false);
+        parabolaOptions.SetActive(false);
+        ellipseOptions.SetActive(false);
+        hyperbolaOptions.SetActive(false);
     }
 }
--- Assets/Scripting/Utility/BolaMathZ.cs
+++ Assets/Scripting/Utility/BolaMathZ.cs
@@ -2,5 +2,30 @@ using UnityEngine;
 
 public class BolaMathZ
 {
-    
+    /*
+    public static Vector2[] GenerateCirclePointmap(float h, float k, float r, int resolution)
+    {
+        Vector2[] pointMap = new Vector2[resolution];
+
+        for (int i = 0; i < resolution; i++)
+        {
+            float angle = i * (360 / resolution);
+        }
+    }
+
+    public static Vector2[] GenerateParabolaPointmap(float h, float k, float p, bool verticalOrHorizontal)
+    {
+
+    }
+
+    public static Vector2[] GenerateElipsePointmap(float h, float k, float a, float b)
+    {
+
+    }
+
+    public static Vector2[] GenerateHyperbolaPointmap(float h, float k, float a, float b, float c, bool verticalOrHorizontal)
+    {
+
+    }
+    */
 }
